a1c1fd5071d4b9dff38c81959419306aa26024ef
ivanstepanov <ivanstepanoff@yahoo.com>
2022-01-16 20:07:40 +0200

    Updates

---
+++ precache-manifest.9d174766abc6e4e31f0bbfa9ed668f79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d933d46ebde4b0a627a381fd5d9c95",
+    "url": "/react-jar/index.html"
+  },
+  {
+    "revision": "f54b984c77b69c748491",
+    "url": "/react-jar/static/css/main.8db01220.chunk.css"
+  },
+  {
+    "revision": "062ff582de024c6b3eb6",
+    "url": "/react-jar/static/js/2.b261a87f.chunk.js"
+  },
+  {
+    "revision": "f54b984c77b69c748491",
+    "url": "/react-jar/static/js/main.afcc2681.chunk.js"
+  },
+  {
+    "revision": "4b026260f527a050c4c4",
+    "url": "/react-jar/static/js/runtime~main.69bcef2f.js"
+  }
+]);
